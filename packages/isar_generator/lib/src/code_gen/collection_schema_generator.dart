57ff3df0a85a49386d5199f576ead31a42830814
milap <milap.c@simformsolutions.com>
2023-02-06 15:49:20 +0530

    fix generating id 5th

--- packages/isar_generator/lib/src/code_gen/collection_schema_generator.dart
+++ packages/isar_generator/lib/src/code_gen/collection_schema_generator.dart
@@ -20,7 +20,7 @@ String generateSchema(ObjectInfo object) {
 
   code += '''
     name: r'${object.isarName}',
-    id: ${BigInt.parse(object.id.toString()).toUnsigned(36)},
+    id: ${BigInt.parse(object.id.toString())/2},
     properties: {$properties},
 
     estimateSize: ${object.estimateSizeName},
